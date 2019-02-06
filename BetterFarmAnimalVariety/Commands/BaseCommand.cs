d84e98a4c8d84b9038b26fd186a60f42f42b9932
Paritee <paritee.hi@gmail.com>
2019-02-05 23:59:29 -0500

    2.1.0
    - Added commands for managing the BFAV config.json file from SMAPI without having to manually update the JSON
    - Added more configurations for determining whether or not a newborn/hatchling can be a type within its category and not just from the parent/producer. If IgnoreParentProduceCheck is off, this means that a white egg could hatch into a Brown Chicken. If randomize is est to true, non-producing animals will be included like bulls and roosters
    - Added format and enable flag to config.json
    - Added versioning and GetBreedFarmAnimal to the API

---
+++ BetterFarmAnimalVariety/Commands/BaseCommand.cs
@@ -0,0 +1,36 @@
+﻿using BetterFarmAnimalVariety.Models;
+using StardewModdingAPI;
+using System.Collections.Generic;
+
+namespace BetterFarmAnimalVariety.Commands
+{
+    class BaseCommand
+    {
+
+        protected List<Command> Commands = new List<Command>();
+
+        protected readonly ModConfig Config;
+        protected readonly IModHelper Helper;
+        protected readonly IMonitor Monitor;
+
+        public BaseCommand(ModConfig config, IModHelper helper, IMonitor monitor)
+        {
+            this.Config = config;
+            this.Helper = helper;
+            this.Monitor = monitor;
+        }
+
+        public void SetUp()
+        {
+            foreach (Command command in this.Commands)
+            {
+                this.Helper.ConsoleCommands.Add(command.Name, command.Documentation, command.Callback);
+            }
+        }
+
+        protected void HandleUpdatedConfig()
+        {
+            this.Helper.WriteConfig<ModConfig>(this.Config);
+        }
+    }
+}
---
+++ BetterFarmAnimalVariety/Models/Command.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterFarmAnimalVariety.Models
+{
+    class Command
+    {
+        public string Name;
+        public string Documentation;
+        public Action<string, string[]> Callback;
+
+        public Command(string name, string documentation, Action<string, string[]> callback)
+        {
+            this.Name = name;
+            this.Documentation = documentation;
+            this.Callback = callback;
+        }
+    }
+}
