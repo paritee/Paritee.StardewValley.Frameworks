7bdd32b5eb39631bac46c56c88705517dbd40bcc
Paritee <paritee.hi@gmail.com>
2019-03-04 11:47:23 -0500

    Display totals in the description if there is a discrepancy and at least one type available for purchase

--- BetterFarmAnimalVariety/Framework/Patches/PurchaseAnimalsMenu/GetAnimalDescription.cs
+++ BetterFarmAnimalVariety/Framework/Patches/PurchaseAnimalsMenu/GetAnimalDescription.cs
@@ -1,4 +1,7 @@
 ﻿using Harmony;
+using System.Collections.Generic;
+using System.Diagnostics;
+using PariteeCore = Paritee.StardewValley.Core;
 
 namespace BetterFarmAnimalVariety.Framework.Patches.PurchaseAnimalsMenu
 {
@@ -11,6 +14,30 @@ namespace BetterFarmAnimalVariety.Framework.Patches.PurchaseAnimalsMenu
             string category = name;
 
             __result = Helpers.FarmAnimals.GetCategory(category).AnimalShop.Description;
+
+            // Add totals to see how many types the player can afford
+            Decorators.Farmer moddedPlayer = new Decorators.Farmer(PariteeCore.Api.Game.GetPlayer());
+
+            List<string> types = Helpers.FarmAnimals.GroupPurchaseableTypesByCategory()[category];
+
+            // Remove blue chickens if needed
+            types = moddedPlayer.SanitizeBlueChickens(types);
+
+            int total = types.Count;
+
+            // Remove any types that the player cannot afford
+            types = moddedPlayer.SanitizeAffordableTypes(types);
+
+            int available = types.Count;
+
+            // Only display totals if there is a discrepancy and at least one 
+            // type available for purchase
+            if (available > 0 && available < total)
+            {
+                // Ex: (1/3 <gold symbol>)
+                string[] substitutions = new string[] { available.ToString(), total.ToString(), "$" };
+                __result += " (" + PariteeCore.Api.Content.LoadString("Strings\\Locations:AdventureGuild_KillList_LineFormat", substitutions) + ")";
+            }
 
             return false;
         }
