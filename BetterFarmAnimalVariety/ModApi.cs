e53437946ac212de7b32d97db615cdeb97f57129
Paritee <paritee.hi@gmail.com>
2019-03-04 21:52:40 -0500

    Removed version checking on API

--- BetterFarmAnimalVariety/ModApi.cs
+++ BetterFarmAnimalVariety/ModApi.cs
@@ -18,21 +18,15 @@ namespace BetterFarmAnimalVariety
             this.ModVersion = modVersion;
         }
 
-        /// <param name="version">string</param>
         /// <returns>Returns bool</returns>
-        public bool IsEnabled(string version)
+        public bool IsEnabled()
         {
-            this.AssertVersionIsSupported(version);
-
             return this.Config.IsEnabled;
         }
 
-        /// <param name="version">string</param>
         /// <returns>Returns List<Paritee.Core.Models.FarmAnimalCategory></returns>
-        public List<PariteeCore.Models.FarmAnimalCategory> GetFarmAnimalCategories(string version)
+        public List<PariteeCore.Models.FarmAnimalCategory> GetFarmAnimalCategories()
         {
-            this.AssertVersionIsSupported(version);
-
             List<PariteeCore.Models.FarmAnimalCategory> categories = new List<PariteeCore.Models.FarmAnimalCategory>();
 
             int order = 0;
@@ -61,25 +55,14 @@ namespace BetterFarmAnimalVariety
         }
 
         /// <summary>Get the farm animal's types from the save data.</summary>
-        /// <param name="version">string</param>
         /// <returns>Returns Dictionary<long, KeyValuePair<string, string>></returns>
-        public Dictionary<long, KeyValuePair<string, string>> GetFarmAnimalTypeHistory(string version)
+        public Dictionary<long, KeyValuePair<string, string>> GetFarmAnimalTypeHistory()
         {
-            this.AssertVersionIsSupported(version);
-
             Framework.Helpers.Assert.GameLoaded();
 
             FarmAnimals saveData = Framework.Helpers.Mod.ReadSaveData<FarmAnimals>(Framework.Constants.Mod.FarmAnimalsSaveDataKey);
 
             return saveData.Animals.ToDictionary(o => o.Id, o => o.TypeLog.ConvertToKeyValuePair());
-        }
-
-        /// <summary>Assert the version asked for is supported.</summary>
-        /// <param name="version">string</param>
-        /// <exception cref="NotSupportedException"></exception>
-        private void AssertVersionIsSupported(string version)
-        {
-            Framework.Helpers.Assert.VersionIsSupported(version, this.ModVersion.MajorVersion.ToString());
         }
     }
 }
