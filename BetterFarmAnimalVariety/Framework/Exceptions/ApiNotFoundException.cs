2711776ed0be0fb0ae012558319fc28850892973
Paritee <paritee.hi@gmail.com>
2019-03-17 11:38:59 -0400

    Fix warnings

--- BetterFarmAnimalVariety/Framework/Exceptions/ApiNotFoundException.cs
+++ BetterFarmAnimalVariety/Framework/Exceptions/ApiNotFoundException.cs
@@ -2,6 +2,7 @@
 
 namespace BetterFarmAnimalVariety.Framework.Exceptions
 {
+    [Serializable]
     class ApiNotFoundException : Exception
     {
         public ApiNotFoundException(string apiKey) : base($"{Constants.Integration.MoreAnimals} API not found") { }
--- BetterFarmAnimalVariety/Framework/Exceptions/SaveNotLoadedException.cs
+++ BetterFarmAnimalVariety/Framework/Exceptions/SaveNotLoadedException.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BetterFarmAnimalVariety.Framework.Exceptions
 {
+    [Serializable]
     class SaveNotLoadedException : Exception
     {
         public SaveNotLoadedException() : base($"Save has not been loaded") { }
